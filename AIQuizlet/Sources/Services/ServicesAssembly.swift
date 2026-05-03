d2229cfbee9f0f1d2fcf65533bbf7f06798577de
zakirka <zakirovazamat0508@gmail.com>
2026-05-03 15:40:12 +0300

    add ServiceAssembly

---
+++ AIQuizlet/Sources/Services/ServicesAssembly.swift
@@ -0,0 +1,30 @@
+//
+//  ServicesAssembly.swift
+//  AIQuizlet
+//
+//  Created by Timur Minkhatov on 01/05/2026.
+//  Copyright © 2026 t-bank-practice-team. All rights reserved.
+//
+
+import SwiftData
+
+final class ServicesAssembly {
+
+    // MARK: - Properties
+
+    let storageService: StorageService
+    let authService: AuthService
+    let quizService: QuizService
+    let cameraService: CameraService
+//    let firestoreService: FirestoreService
+
+    // MARK: - Init
+
+    init(modelContainer: ModelContainer) {
+        self.storageService = StorageService(modelContainer: modelContainer)
+        self.authService = AuthService.shared
+        self.quizService = QuizService(networkManager: NetworkManager())
+        self.cameraService = CameraService()
+//        self.firestoreService = FirestoreService()
+    }
+}
